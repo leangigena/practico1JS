8bab1477d1a630843b36e3db48d9ab880a72ba1c
Leandro Agustín Gigena <gigelean@gmail.com>
2021-07-02 11:13:04 -0300

    Primer Practico JS

---
+++ js/practico1.js
@@ -0,0 +1,6 @@
+let nombre = prompt ("Hola! ¿Cómo te llamas?");
+alert ("Bienvenido" + " " + nombre + "!" + " " + "Hagamos un ejercicio")
+let num1 = parseInt(prompt("Ingresá algún número que se te ocurra"));
+let num2 = parseInt(prompt("Por favor, escribí otro número"));
+let suma = num1 + num2;
+console.log("Si los sumás da:" + " ", suma);
